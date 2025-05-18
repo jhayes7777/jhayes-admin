6ce72bd557025a28f9c11286507869b912fff1f1
jhayes7777 <310869927@qq.com>
2025-05-18 19:20:45 +0800

    online

--- web/vite.config.js
+++ web/vite.config.js
@@ -109,7 +109,7 @@ export default ({ mode }) => {
         ]
       }),
       vuePlugin(),
-      svgBuilder(['./src/plugin/','./src/assets/icons/'],base, outDir,'assets', NODE_ENV),
+      // svgBuilder(['./src/plugin/','./src/assets/icons/'],base, outDir,'assets', NODE_ENV),
       [Banner(`\n Build based on gin-vue-admin \n Time : ${timestamp}`)],
       VueFilePathPlugin('./src/pathInfo.json')
     ]
